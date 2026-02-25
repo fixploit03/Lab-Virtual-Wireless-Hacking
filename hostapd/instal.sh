ef65c8c05318107f0232b9b4b7f425e7b597279b
fixploit03 <164528883+fixploit03@users.noreply.github.com>
2026-02-25 23:34:16 +0700

    Create instal.sh

---
+++ hostapd/instal.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# update repositori linux
+apt-get update
+
+# list dependensi
+dependensi=(
+	"build-essential"
+	"pkg-config"
+	"libssl-dev"
+	"libnl-3-dev"
+	"libnl-genl-3-dev"
+	"libnl-route-3-dev"
+)
+
+# instal dependensi
+for d in "${dependensi[@]}"; do
+  apt-get install "${d}"
+done
+
+tar -zxf hostapd-2.11.tar.gz
+cd hostapd-2.11/hostapd
+make -j$(nproc)
+make install SBINDIR=/usr/sbin BINDIR=/usr/sbin
